3366110fcf9aac212b2b024410c71b6b6c182c0d
Joao Victor <joaobrito265@outlook.com>
2023-10-31 12:56:50 -0300

    segundo commit

--- crud1/config.php
+++ crud1/config.php
@@ -1,7 +1,7 @@
 <?php 
     define('HOST', 'localhost');
     define('USER', 'root');
-    define('PASS','jv982314');
+    define('PASS','yourpassword_here');
     define('BASE', 'cadastro');
 
     $conexao = new mysqli(HOST,USER,PASS,BASE, 3312);
